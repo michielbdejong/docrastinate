e5cc44ab2a660a7cde1c7c1337e1e33e6e0d3520
Michiel de Jong <michiel@unhosted.org>
2012-08-05 13:09:44 +0200

    add remoteStorage.js

--- js/docrastinate.js
+++ js/docrastinate.js
@@ -1,43 +1,43 @@
 $(document).ready(function () {
-	'use strict';
+  'use strict';
 
-	// load values and scroll position on start
-	$('#calendar').val(localStorage.getItem('calendar'));
-	$('#projects').val(localStorage.getItem('projects'));
-	$('#personal').val(localStorage.getItem('personal'));
-	$('#calendar').scrollTop(localStorage.getItem('scroll-calendar'));
-	$('#projects').scrollTop(localStorage.getItem('scroll-projects'));
-	$('#personal').scrollTop(localStorage.getItem('scroll-personal'));
+  // load values and scroll position on start
+  $('#calendar').val(localStorage.getItem('calendar'));
+  $('#projects').val(localStorage.getItem('projects'));
+  $('#personal').val(localStorage.getItem('personal'));
+  $('#calendar').scrollTop(localStorage.getItem('scroll-calendar'));
+  $('#projects').scrollTop(localStorage.getItem('scroll-projects'));
+  $('#personal').scrollTop(localStorage.getItem('scroll-personal'));
 
-	syncer.display('remotestorage-connect', ['tasks'], 'syncer/', function (e) {
-		$('#calendar').val(syncer.getItem('tasks', 'calendar'));
-		$('#projects').val(syncer.getItem('tasks', 'projects'));
-		$('#personal').val(syncer.getItem('tasks', 'personal'));
-	});
+  remoteStorage.displayWidget('remotestorage-connect', 'en');
+  remoteStorage.loadModule('documents', 'rw');
+  var notes = remoteStorage.documents.getPrivateList('notes');
+  notes.on('change', function (e) {
+    $('#calendar').val(notes.getContent('calendar'));
+    $('#projects').val(notes.getContent('projects'));
+    $('#personal').val(notes.getContent('personal'));
+  });
 
-	$('#calendar').change(function () {
-		syncer.setItem('tasks', 'calendar', $('#calendar').val());
-		localStorage.setItem('calendar', $('#calendar').val());
-		localStorage.setItem('scroll-calendar', $('#calendar').scrollTop());
-	});
-	$('#projects').change(function () {
-		syncer.setItem('tasks', 'projects', $('#projects').val());
-		localStorage.setItem('projects', $('#projects').val());
-		localStorage.setItem('scroll-projects', $('#projects').scrollTop());
-	});
-	$('#personal').change(function () {
-		syncer.setItem('tasks', 'personal', $('#personal').val());
-		localStorage.setItem('personal', $('#personal').val());
-		localStorage.setItem('scroll-personal', $('#personal').scrollTop());
-	});
+  $('#calendar').change(function () {
+    notes.setContent('calendar', $('#calendar').val());
+    localStorage.setItem('scroll-calendar', $('#calendar').scrollTop());
+  });
+  $('#projects').change(function () {
+    notes.setContent('projects', $('#projects').val());
+    localStorage.setItem('scroll-projects', $('#projects').scrollTop());
+  });
+  $('#personal').change(function () {
+    notes.setContent('personal', $('#personal').val());
+    localStorage.setItem('scroll-personal', $('#personal').scrollTop());
+  });
 });
 
 $(window).unload(function () { // save values and scroll position on exit
-	'use strict';
-	localStorage.setItem('calendar', $('#calendar').val());
-	localStorage.setItem('projects', $('#projects').val());
-	localStorage.setItem('personal', $('#personal').val());
-	localStorage.setItem('scroll-calendar', $('#calendar').scrollTop());
-	localStorage.setItem('scroll-projects', $('#projects').scrollTop());
-	localStorage.setItem('scroll-personal', $('#personal').scrollTop());
+  'use strict';
+  notes.setContent('calendar', $('#calendar').val());
+  notes.setContent('projects', $('#projects').val());
+  notes.setContent('personal', $('#personal').val());
+  localStorage.setItem('scroll-calendar', $('#calendar').scrollTop());
+  localStorage.setItem('scroll-projects', $('#projects').scrollTop());
+  localStorage.setItem('scroll-personal', $('#personal').scrollTop());
 });
